4c8509a7990fc95a03ef21a63ab2bfe00258d9b9
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-10-17 16:50:29 +0900

    🔧 set react config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,44 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import js from "@eslint/js";
+import typescript from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescript,
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin
+    },
+    rules: {
+      ...typescript.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-no-target-blank': ['error', { enforceDynamicLinks: 'always' }],
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
 ];
